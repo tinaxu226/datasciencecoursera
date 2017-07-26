15a1a8b8399f55c200082ebfddebdd3c31e84a85
Ting Xu <tinaxu226@gmail.com>
2017-07-26 13:40:53 -0400

    Add files via upload

---
+++ coursera-exploratory-data-analysis-course-project-1-master/Plot 3.R
@@ -0,0 +1,13 @@
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
+sub <- df[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(sub$Date, sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(sub$Global_active_power)
+subMetering1 <- as.numeric(sub$Sub_metering_1)
+subMetering2 <- as.numeric(sub$Sub_metering_2)
+subMetering3 <- as.numeric(sub$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", xlab="", ylab="Energy Submetering")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5,col=c("black", "red", "blue"))
